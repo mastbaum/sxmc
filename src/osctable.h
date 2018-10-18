c97b979bbb1b08ce6df10d9a456c5fbbfc0c9f0b
Karin <gilje@ualberta.ca>
2018-10-18 13:02:35 -0400

    Removing deprecated functions from the osctable class.

--- src/osctable.h
+++ src/osctable.h
@@ -24,46 +24,13 @@ class OscTable {
   std::string filename;
   std::string nutype;
 
+  /**
+   * Calculates the minimum, maximum of an input vector and the number of steps and step size.
+   *
+   * \param inputVector
+   * \return min max nsteps and step passed by reference
+   */
   void GetParLimits(std::vector<double> inputVector, double &min, double &max, double &nsteps, double &step);
-
-  // These functions will not be used... just here for reference?
-
-  /**
-   * Calculates the Index that matches the pulled tan2 theta12.
-   *
-   * \param inTheta - the input pull of tan2 theta 12
-   * \return index of bin for inTheta in tan2 theta 12 array.
-   */
-  int FindThetaIndex(double inTheta);
-  
-  /**
-   * Calculates the Index that matches the pulled Delta m2 21
-   *
-   * \param inMass - the input pull of Delta m2 21
-   * \return index of bin for inMass in Delta m2 21 Array.
-   */
-  int FindMassIndex(double inMass);
-  
-  /**
-   * Calculates the Index that matches the Energy
-   *
-   * \param inE = the energy to lookup
-   * \return index of bin for inE in Energy array
-   */
-  int FindEnergyIndex(double inE);
-  
-  /**
-   * Calculates the global array Index that matches the 
-   * tan2 theta 12, Delta m2 21 and energy indices
-   *
-   * \param iThetaIndex - the index of tan2 theta 12 in the tan2 theta 12 array
-   * \param iMassIndex - the index of Delta m2 21 in the Delta m2 21 array.
-   * \param iEIndex - the index of the energy in the energy array
-   * \return global index of full array
-   */
-  int CalculateTotalIndex(int iThetaIndex, int iMassIndex, int iEIndex);
-
-  double Reweight(double dTheta, double dMass, double dEnergy);
   
 };
 
